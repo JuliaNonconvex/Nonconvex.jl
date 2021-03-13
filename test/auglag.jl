4ee82f220d4b723d0dda07187a43d331494edb5f
Mohamed Tarek <mohamed82008@gmail.com>
2021-03-14 04:39:11 +1100

    simplify AugLag test

--- test/auglag.jl
+++ test/auglag.jl
@@ -4,8 +4,6 @@ const FDM = FiniteDifferences
 f(x::AbstractVector) = sqrt(x[2])
 g(x::AbstractVector, a, b) = (a*x[1] + b)^3 - x[2]
 
-options = Nonconvex.MMAOptions(tol = Nonconvex.Tolerance(kkt = 1e-6, f = 0.0))
-
 m = Nonconvex.Model(f)
 addvar!(m, [1e-4, 1e-4], [10.0, 10.0])
 add_ineq_constraint!(
@@ -14,13 +12,9 @@ add_ineq_constraint!(
 )
 x0 = [2.0, 2.0]
 
-alg1 = MMA02()
-r1 = optimize(m, alg1, x0)
-@test abs(r1.minimum - sqrt(8/27)) < 1e-4
-@test norm(r1.minimizer - [1/3, 8/27]) < 1e-4
-
-#alg2 = AugLag(primaloptimizer = MMA87())
-alg2 = AugLag()
-r2 = optimize(m, alg2, x0)
-@test abs(r2.minimum - sqrt(8/27)) < 1e-4
-@test norm(r2.minimizer - [1/3, 8/27]) < 1e-4
+#alg = AugLag(primaloptimizer = MMA87())
+alg = AugLag()
+options = Nonconvex.AugLagOptions(alg)
+r = optimize(m, alg, x0, options = options)
+@test abs(r.minimum - sqrt(8/27)) < 1e-4
+@test norm(r.minimizer - [1/3, 8/27]) < 1e-4
