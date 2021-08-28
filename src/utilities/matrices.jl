6550df7b9318824a0a40cee4c88ac67b56013a3c
Ping <ping69852@gmail.com>
2021-08-28 16:53:04 -0500

    unused functions

--- src/utilities/matrices.jl
+++ src/utilities/matrices.jl
@@ -1,31 +1,4 @@
 # Funcs
-function is_triangular(n)
-    if (n < 0)
-        return false
-    end
-    c = -2 * n
-    a, b = 1, 1
-    d = (b * b) - (4 * a * c)
-    if (d < 0)
-        return false
-    end
-    root1 = ( -b + sqrt(d)) / (2 * a)
-    root2 = ( -b - sqrt(d)) / (2 * a)
- 
-    if (root1 > 0 && floor(root1) == root1)
-        return true
-    elseif (root2 > 0 && floor(root2) == root2)
-        return true
-    else
-        return false
-    end
-end
-
-function length_to_dim(l)
-    @assert isinteger(l) "Dimension numbers shoule be a integer. "
-    isqrt(l)
-end
-
 function lowertriangind(mat::Matrix)
     indices = [i for i in CartesianIndices(mat) if i[1]>i[2]]
     return LinearIndices(mat)[indices]
