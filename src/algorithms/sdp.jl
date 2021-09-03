de65c4dad26d3d413b1edcac47a21eba7a9c9df0
Ping <ping69852@gmail.com>
2021-09-03 00:01:35 -0500

    fix compile error

--- src/algorithms/sdp.jl
+++ src/algorithms/sdp.jl
@@ -17,7 +17,15 @@ end
 ```
     decompress_symmetric
 
-    For example: a 3*3 positive semidefinite matrix [a b d; b c e; d e f] represents by [x_D[1] x_L[3] x_L[2]; x_L[1] x_D[2] x_L[1]; x_L[2] x_L[3] x_D[3]]
+    For example: 
+    a 3*3 positive semidefinite matrix: 
+    `[  a       b       d; 
+        b       c       e; 
+        d       e       f       ]` 
+    represents by: 
+    `[  x_D[1]  x_L[3]  x_L[2]; 
+        x_L[1]  x_D[2]  x_L[1]; 
+        x_L[2]  x_L[3]  x_D[3]  ]``
 
 - `x_L::AbstractArray`: representing lower triangular part of a `n*n` matrix, length should be `(n^2-n)÷2`
 - `x_D::AbstractArray`: representing diagonal part of a `n*n` matrix, length should be `n`
