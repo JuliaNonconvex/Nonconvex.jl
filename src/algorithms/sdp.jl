4f405a0745bad4f71b4a1af9fce14a7236412222
Ping <ping69852@gmail.com>
2021-09-03 00:10:16 -0500

    fix compile error

--- src/algorithms/sdp.jl
+++ src/algorithms/sdp.jl
@@ -25,7 +25,7 @@ end
     represents by: 
     `[  x_D[1]  x_L[3]  x_L[2]; 
         x_L[1]  x_D[2]  x_L[1]; 
-        x_L[2]  x_L[3]  x_D[3]  ]``
+        x_L[2]  x_L[3]  x_D[3]  ]`
 
 - `x_L::AbstractArray`: representing lower triangular part of a `n*n` matrix, length should be `(n^2-n)÷2`
 - `x_D::AbstractArray`: representing diagonal part of a `n*n` matrix, length should be `n`
