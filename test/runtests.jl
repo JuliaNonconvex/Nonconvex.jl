88c5d07dc11bb70b57f3eb57663082e0b1295b93
Mohamed Tarek <mohdibntarek@gmail.com>
2026-02-10 17:01:34 +0400

    skip SafeTestsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,93 +1,91 @@
-using Test, SafeTestsets
+using Test, SafeTestsets, Nonconvex, Pkg
 
 const GROUP = get(ENV, "GROUP", "All")
 
-using Test, Nonconvex, Pkg
-
 if GROUP == "All" || GROUP == "NonconvexIpopt"
-    @safetestset "NonconvexIpopt" begin
+    @testset "NonconvexIpopt" begin
         @test_throws ArgumentError using NonconvexIpopt
         Nonconvex.@load Ipopt
         IpoptAlg()
     end
 elseif GROUP == "All" || GROUP == "NonconvexNLopt"
-    @safetestset "NonconvexNLopt" begin
+    @testset "NonconvexNLopt" begin
         @test_throws ArgumentError using NonconvexNLopt
         Nonconvex.@load NLopt
         NLoptAlg(:LD_MMA)
     end
 elseif GROUP == "All" || GROUP == "NonconvexJuniper"
-    @safetestset "NonconvexJuniper" begin
+    @testset "NonconvexJuniper" begin
         @test_throws ArgumentError using NonconvexJuniper
         Nonconvex.@load Juniper
         JuniperIpoptAlg()
         IpoptAlg()
     end
 elseif GROUP == "All" || GROUP == "NonconvexMMA"
-    @safetestset "NonconvexMMA" begin
+    @testset "NonconvexMMA" begin
         @test_throws ArgumentError using NonconvexMMA
         Nonconvex.@load MMA
         MMA87()
         MMA02()
     end
 elseif GROUP == "All" || GROUP == "NonconvexPavito"
-    @safetestset "NonconvexPavito" begin
+    @testset "NonconvexPavito" begin
         @test_throws ArgumentError using NonconvexPavito
         Nonconvex.@load Pavito
         PavitoIpoptCbcAlg()
         IpoptAlg()
     end
 elseif GROUP == "All" || GROUP == "NonconvexBayesian"
-    @safetestset "NonconvexBayesian" begin
+    @testset "NonconvexBayesian" begin
         @test_throws ArgumentError using NonconvexBayesian
         Nonconvex.@load Bayesian
         BayesOptAlg(IpoptAlg())
     end
 elseif GROUP == "All" || GROUP == "NonconvexAugLagLab"
-    @safetestset "NonconvexAugLagLab" begin
+    @testset "NonconvexAugLagLab" begin
         @test_throws ArgumentError using NonconvexAugLagLab
         Nonconvex.@load AugLag2
         AugLag2()
     end
 elseif GROUP == "All" || GROUP == "NonconvexSemidefinite"
-    @safetestset "NonconvexSemidefinite" begin
+    @testset "NonconvexSemidefinite" begin
         @test_throws ArgumentError using NonconvexSemidefinite
         Nonconvex.@load Semidefinite
         SDPBarrierAlg(IpoptAlg())
     end
 elseif GROUP == "All" || GROUP == "NonconvexSearch"
-    @safetestset "NonconvexSearch" begin
+    @testset "NonconvexSearch" begin
         @test_throws ArgumentError using NonconvexSearch
         Nonconvex.@load Search
         MTSAlg()
         LS1Alg()
     end
 elseif GROUP == "All" || GROUP == "NonconvexTOBS"
-    @safetestset "NonconvexTOBS" begin
+    @testset "NonconvexTOBS" begin
         @test_throws ArgumentError using NonconvexTOBS
         Nonconvex.@load TOBS
         TOBSAlg()
     end
 elseif GROUP == "All" || GROUP == "NonconvexMetaheuristics"
-    @safetestset "NonconvexMetaheuristics" begin
+    @testset "NonconvexMetaheuristics" begin
         @test_throws ArgumentError using NonconvexMetaheuristics
         Nonconvex.@load Metaheuristics
         MetaheuristicsAlg(ECA)
     end
 elseif GROUP == "All" || GROUP == "NonconvexNOMAD"
-    @safetestset "NonconvexNOMAD" begin
+    @testset "NonconvexNOMAD" begin
         @test_throws ArgumentError using NonconvexNOMAD
         Nonconvex.@load NOMAD
         NOMADAlg()
     end
 elseif GROUP == "All" || GROUP == "NonconvexMultistart"
-    @safetestset "NonconvexMultistart" begin
+    @testset "NonconvexMultistart" begin
         @test_throws ArgumentError using NonconvexMultistart
         Nonconvex.@load Multistart
         HyperoptAlg(IpoptAlg())
     end
 elseif GROUP == "All" || GROUP == "NonconvexPercival"
-    @safetestset "NonconvexPercival" begin
+    @testset "NonconvexPercival" begin
         @test_throws ArgumentError using NonconvexPercival
         Nonconvex.@load AugLag
         AugLag()
